db764e4335dc8a72449a4d200d08398bddce17f4
daniel8298 <daniel8298@gmail.com>
2023-12-28 11:16:26 +0200

    newProject

--- server/app/src/graphql/logger/apolloLogger.ts
+++ server/app/src/graphql/logger/apolloLogger.ts
@@ -1,17 +1,17 @@
-// import chalk from "chalk";
+import chalk from "chalk";
 import { GraphQLRequestContext, BaseContext } from "@apollo/server";
 
 const apolloLogger = {
   async requestDidStart({ request }: GraphQLRequestContext<BaseContext>) {
-    request.http;
-    // &&
-    //   console.log(
-    //     chalk.cyanBright(
-    //       `[${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}] ${
-    //         request.http.method
-    //       } ${request.query}`
-    //     )
-    //   );
+    request.http &&
+      request.operationName !== "IntrospectionQuery" &&
+      console.log(
+        chalk.cyanBright(
+          `[${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}] ${
+            request.http.method
+          } ${request.query}`
+        )
+      );
   },
 };
 
